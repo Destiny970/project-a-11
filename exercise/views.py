75ed59e31e4cde5868343ebfd9eb8af79804d7cd
Piers Kirk <pfk3jd@virginia.edu>
2021-03-15 00:16:08 -0400

    Update views.py
    added views to handle different web site branches.

--- exercise/views.py
+++ exercise/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ExerciseForm
+from .models import Exercise
 
-# Create your views here.
+def home(request):
+    return render(request, 'exercise/HomeLogin.html')
+
+def user_home(request):
+    return render(request, 'exercise/UserHome.html')
+
+def log_nws(request):
+    if request.method == 'POST':
+        filled_form = ExerciseForm(request.POST)
+        print(filled_form.errors)
+        if filled_form.is_valid():
+            filled_form.save() 
+            note = 'Thank you, your workout has been logged.'
+        else: 
+            note = "Something went wrong, please try again."
+        new_form = ExerciseForm()
+        return render(request, 'exercise/LogNW.html', {'exerciseform':new_form, 'note':note})
+    else: 
+        form = ExerciseForm()
+        return render(request, 'exercise/LogNW.html', {'exerciseform': form})
+    
+def my_ws(request):
+    form = ExerciseForm()
+    exercise = Exercise.objects.all()
+    args = {'form': form, 'exercise': exercise}
+    return render(request, 'exercise/MyWorkouts.html', args)
+
+def my_points(request):
+    return render(request, 'exercise/MyPoints.html')
+
