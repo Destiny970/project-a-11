0c8a40cb3cad26b0c389af0d4161f379a937aae4
kattaylor05 <kmt2ns@virginia.edu>
2021-03-17 11:22:57 -0400

    some (imperfect) html additions for home page

--- project/urls.py
+++ project/urls.py
@@ -6,10 +6,9 @@ from django.views.generic import TemplateView
 app_name = 'exercise'
 urlpatterns = [
     # added 03/01/21
-    path('', TemplateView.as_view(template_name = "exercise/index.html")),
+    path('', TemplateView.as_view(template_name = "exercise/HomeLogin.html")),
     # added 03/01/21
     path('accounts/', include('allauth.urls')),
-    
     path('exercise/', include('exercise.urls')),
     path('admin/', admin.site.urls),
 ]
