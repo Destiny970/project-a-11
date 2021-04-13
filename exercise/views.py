30a6e9dbc461a2a8332c305280bc29c6540d1186
acl8cy <77541922+acl8cy@users.noreply.github.com>
2021-04-12 22:47:02 -0400

    Dropdown Navigation Bar

--- exercise/views.py
+++ exercise/views.py
@@ -132,18 +132,21 @@ def the_weather(request):
         form.save()
 
     form = CityForm()
+    r = requests.get(url.format(city))
     # request the API data and convert the JSON to Python data types
     for city in cities:
-        city_weather = requests.get(url.format(city)).json()
+        city_weather = requests.get(url.format(city.name)).json()
         weather = {
-            'city': city,
+            'city': city_weather['name'],
             'temperature': city_weather['main']['temp'],
             'description': city_weather['weather'][0]['description'],
             'icon': city_weather['weather'][0]['icon']
         }
         weather_data.append(weather)
-    context = {'weather_data': weather_data, 'form': form}
-    return render(request, 'exercise/weather.html', context)
+        context = {'weather_data': weather_data, 'form': form}
+        return render(request, 'exercise/weather.html', context)
+    else:
+        return HttpResponse("Not Found")
 
 
 def index(request):
