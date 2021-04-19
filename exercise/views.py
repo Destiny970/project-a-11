1b51f274121bd1a57c09a7b17cc88d543adbe114
kattaylor05 <kmt2ns@virginia.edu>
2021-04-19 08:54:11 -0400

    test commit - server being weird

--- exercise/views.py
+++ exercise/views.py
@@ -1,3 +1,4 @@
+# adding comment to test commit
 from django.db.models import Sum
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotAllowed
